8de8bc6045309485683951660a63ce20a6aad059
raghunathnv <raghunathnv2020@gmail.com>
2020-01-07 18:22:46 -0500

    first commit

---
+++ src/test/java/artifactid/Mobile.java
@@ -0,0 +1,25 @@
+package artifactid;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Mobile {
+
+	@Test
+	public void MobileLogin()
+	{
+		System.out.println("Mobile Login");
+	}
+	
+	@Test
+	public void MobilePurchase()
+	{
+		System.out.println("Mobile Purchase");
+	}
+	
+	@Test
+	public void MobileLogout()
+	{
+		System.out.println("Mobile Logout");
+	}
+}
